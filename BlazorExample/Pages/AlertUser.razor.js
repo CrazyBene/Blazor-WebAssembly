04183182961a60f23d4a7736ccfb98e579f9b866
CrazyBene <benedikt.grau@gmx.de>
2024-11-04 16:15:45 +0100

    js-interop

---
+++ BlazorExample/Pages/AlertUser.razor.js
@@ -0,0 +1,8 @@
+export function alertUser() {
+  alert("The button was selected!")
+}
+
+export function addHandlers() {
+  const btn = document.getElementById("btn")
+  btn.addEventListener("click", alertUser)
+}
